cd576d582842e7dd5d99864f421fac902c345281
TravisM <mckinstrytg1@gmail.com>
2021-01-26 12:38:12 -0700

    Creates login page, passes spec

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get '/register', to: 'users#new'
   post '/users', to: 'users#create'
   get '/dashboard', to: 'users#show'
+  get '/login', to: 'sessions#new'
 end
