3100607e69a34ad6fa644fbb374c21e29afc2872
TravisM <mckinstrytg1@gmail.com>
2021-01-25 17:22:54 -0700

    Passes User registers then redirects to dashboard, creates session/current_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,24 @@
 
 class UsersController < ApplicationController
   def new; end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/dashboard'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @user = current_user
+  end
+
+  private
+
+  def user_params
+    params.permit(:first_name, :last_name, :email, :password)
+  end
 end
