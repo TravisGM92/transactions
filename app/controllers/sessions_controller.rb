c84165277612772ba846af67cd5a7dfd6747c2d9
TravisM <mckinstrytg1@gmail.com>
2021-01-26 12:58:54 -0700

    Creates and passes spec for logging in with wrong credentials

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,9 @@ class SessionsController < ApplicationController
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to('/dashboard')
+    else
+      flash[:error] = 'Sorry your credentials are bad, try again'
+      redirect_to('/login')
     end
   end
 end
--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -20,8 +20,15 @@ RSpec.describe 'Login page' do
   end
   describe 'if user logs in with bad credentials' do
     it 'user is redirected to login page with error message' do
+      user = User.create!({email: '1234@email.com', first_name: 'George', last_name: 'Canary', password: '1234'})
       visit('/login')
 
+      fill_in :email, with: user.email
+      fill_in :password, with: '1111'
+      click_button('Login')
+      expect(current_path).to_not eq('/dashboard')
+      expect(current_path).to eq('/login')
+      expect(page).to have_content('Sorry your credentials are bad, try again')
     end
   end
 end
